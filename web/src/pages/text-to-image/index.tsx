3eb57b2de8b6c13c92c94964282c1814aec7b6bf
xingjiang.luo <1369615207@qq.com>
2026-03-16 14:08:07 +0800

    update:新增xinference配置 update:修改嵌入网站代码

--- web/src/pages/text-to-image/index.tsx
+++ web/src/pages/text-to-image/index.tsx
@@ -1,11 +1,12 @@
-const UDIFY_IFRAME_SRC = 'http://127.0.0.1:3000';
+const getIframeSrc = () =>
+  `${typeof window !== 'undefined' ? window.location.protocol : 'http:'}//${typeof window !== 'undefined' ? window.location.hostname : 'localhost'}:3300`;
 
 export default function TextToImagePage() {
   return (
     <section className="flex-1 flex flex-col min-h-0 w-full overflow-hidden">
       <div className="relative w-full h-full flex-1 min-h-0">
         <iframe
-          src={UDIFY_IFRAME_SRC}
+          src={getIframeSrc()}
           title="文生图"
           className="w-full h-full border-0"
           allow="microphone"
